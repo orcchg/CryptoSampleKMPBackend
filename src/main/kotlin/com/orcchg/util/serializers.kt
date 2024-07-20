0bbb4bbf6181fa33912faea887ad5a05db036cbd
orcchg <prism.getprism@gmail.com>
2024-07-20 12:43:16 +0400

    Style

--- src/main/kotlin/com/orcchg/util/serializers.kt
+++ src/main/kotlin/com/orcchg/util/serializers.kt
@@ -12,35 +12,31 @@ import java.util.Currency
 
 object BigDecimalSerializer : KSerializer<BigDecimal> {
 
-    override val descriptor: SerialDescriptor = PrimitiveSerialDescriptor("decimal", PrimitiveKind.STRING)
+    override val descriptor: SerialDescriptor = PrimitiveSerialDescriptor("com.orcchg.util.BigDecimalSerializer", PrimitiveKind.STRING)
 
     override fun serialize(encoder: Encoder, value: BigDecimal) {
         encoder.encodeString(value.abs().toPlainString())
     }
 
-    override fun deserialize(decoder: Decoder): BigDecimal {
-        val string = decoder.decodeString()
-        return BigDecimal(string)
-    }
+    override fun deserialize(decoder: Decoder): BigDecimal =
+        BigDecimal(decoder.decodeString())
 }
 
 object CurrencySerializer : KSerializer<Currency> {
 
-    override val descriptor: SerialDescriptor = PrimitiveSerialDescriptor("currency", PrimitiveKind.STRING)
+    override val descriptor: SerialDescriptor = PrimitiveSerialDescriptor("com.orcchg.util.CurrencySerializer", PrimitiveKind.STRING)
 
     override fun serialize(encoder: Encoder, value: Currency) {
         encoder.encodeString(value.currencyCode)
     }
 
-    override fun deserialize(decoder: Decoder): Currency {
-        val currencyCode = decoder.decodeString()
-        return Currency.getInstance(currencyCode)
-    }
+    override fun deserialize(decoder: Decoder): Currency =
+        Currency.getInstance(decoder.decodeString())
 }
 
 object SignSerializer : KSerializer<MoneySign> {
 
-    override val descriptor: SerialDescriptor = PrimitiveSerialDescriptor("sign", PrimitiveKind.STRING)
+    override val descriptor: SerialDescriptor = PrimitiveSerialDescriptor("com.orcchg.util.SignSerializer", PrimitiveKind.STRING)
 
     override fun serialize(encoder: Encoder, value: MoneySign) {
         val ch = when (value) {
@@ -50,12 +46,10 @@ object SignSerializer : KSerializer<MoneySign> {
         encoder.encodeString(ch)
     }
 
-    override fun deserialize(decoder: Decoder): MoneySign {
-        val ch = decoder.decodeString()
-        return when (ch) {
+    override fun deserialize(decoder: Decoder): MoneySign =
+        when (decoder.decodeString()) {
             "-" -> MoneySign.MINUS
             "+" -> MoneySign.PLUS
             else -> MoneySign.PLUS
         }
-    }
 }
